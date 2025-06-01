2bcee29445679d7395ca69a6d03d09d14cd15278
nkumbala129 <narenderkumbalak@gmail.com>
2025-06-01 09:26:04 +0530

    Update ProcCortexAIAssistant.py

--- ProcCortexAIAssistant.py
+++ ProcCortexAIAssistant.py
@@ -115,7 +115,7 @@ st.markdown("""
     right: 0px;
     z-index: 1000;
     width: 150px;
-    height: auto;
+    height: 100px;
 }
 /* Style for the top logo */
 .top-logo {
@@ -124,7 +124,7 @@ st.markdown("""
     right: 150px;
     z-index: 1000;
     width: 900px;
-    height: auto;
+    height: 100px;
 }
 /* Theme-specific styling */
 body {

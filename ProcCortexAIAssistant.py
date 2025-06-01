81b0b4c4674c5f04823de51f09f3922294c4827e
nkumbala129 <narenderkumbalak@gmail.com>
2025-06-01 08:04:31 +0530

    Update ProcCortexAIAssistant.py

--- ProcCortexAIAssistant.py
+++ ProcCortexAIAssistant.py
@@ -120,7 +120,7 @@ st.markdown("""
 .top-logo {
     position: fixed;
     top: 10px; /* Position below Dilytics logo to avoid overlap */
-    left: 270px;
+    left: 280px;
     z-index: 1000; /* Same z-index to stay above other elements */
     width: 250px; /* Adjust size as needed */
     height: auto;
